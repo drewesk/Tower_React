d46d070b329ae7dbbda4609b483e643d54e70a8d
Matthew Gonzer <matthewgonzer@gmail.com>
2017-07-28 14:24:07 -0600

    location

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class Location extends Component {
   constructor(props) {
     super();
-    
+
     this.state = {
 
     }
@@ -11,7 +11,15 @@ export class Location extends Component {
 
   render() {
     return(
-      <div className="location-container">
+      <div className="location-container container">
+        <section className='row'>
+          <div id='location-image' className='col-6'>
+            <img id="mordor" src="../assets/images/MordorMap.jpg"></img>
+          </div>
+          <div id="location-description" className='col-6'>
+            <p>The Tower Dive Bar is conveniently located in central Mordor. Follow the eye of Sauron and find us next to the fire pit.</p>
+          </div>
+        </section>
       </div>
     );
   }
